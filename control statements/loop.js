cd2a1cdcf4994732c90d2f11bf004674c383508b
bharathnadar <mailbharath.889@gmail.com>
2024-06-26 23:16:44 +0530

    loop added

---
+++ control statements/loop.js
@@ -0,0 +1,10 @@
+// let a=20;
+// for(i=1; i<=5; i++){
+//     document.writeln(a);
+// }
+// for (i=20;i<=100;i+=20){
+//     document.writeln(i);
+// }
+for (i=1;i<=10;i++){
+    document.writeln(`3 * ${i} = ${3*i}`);
+}
